bd6dcd364f6b32371720b9d54aaf24c6b2940e88
Byungju Kim <74722197+sh596@users.noreply.github.com>
2022-10-25 18:41:07 +0900

    fix : Record page back button

--- app/src/main/java/com/example/benjamin/MainActivity.kt
+++ app/src/main/java/com/example/benjamin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.benjamin
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.benjamin.base.BaseActivity
 import com.example.benjamin.databinding.ActivityMainBinding
 import com.example.benjamin.viewmodel.MainViewModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -23,6 +29,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
         viewmodel.getData()
 
 
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Fetch failed",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     fun clickMainVirtue(view: View) {
